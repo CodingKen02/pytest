8d6fa9e702c9c28af286bc25c1a8a014484046e6
CodingKen02 <71536021+CodingKen02@users.noreply.github.com>
2023-01-07 00:28:08 -0600

    Partner finished last 4 functions...
    Uploaded partners 4 test functions on the original file, and created a section for partner to use on the test_correctedFunctions file. This is where they will insert their try/except blocks.
    
    Co-Authored-By: ITJavierIDavis <122136754+ITJavierIDavis@users.noreply.github.com>

--- test_correctedFunctions.py
+++ test_correctedFunctions.py
@@ -1,5 +1,5 @@
 """
-Created by: Kennedy Keyes and Javier Davis (add netIDs!)
+Created by: Kennedy Keyes (kfk38) and Javier Davis (insert netID)
 Assignment: Software Testing Group Project
 Course: CSE Methods and Tools in Software Development
 Program: Corrections for test functions (some are intended to still fail)
@@ -110,7 +110,7 @@ def isPalindrome(temp):
 ## isPalindrome test function with 2 distinct tests (palindrome correctness & data type) below:
 
 ## Palindrome Correctness: the first 2 tests-> 1st passes/2nd fails
-## Data Types: the last 4 tests-> 3rd fails/4th fails/5th fails/6th fails/7th passes
+## Data Types: the last 5 tests-> 3rd fails/4th fails/5th fails/6th fails/7th passes
 ## (Data Types used: integer/boolean/complex number/float/string)
 
 @pytest.mark.parametrize("strings, boolean", [("racecar", True), ("anna", False), 
@@ -118,7 +118,168 @@ def isPalindrome(temp):
 def test_isPalindrome(strings, boolean):
     assert isPalindrome(strings) == boolean
 
+
+##-------------------------------------------------------------------------- START CORRECTING BELOW JD:
+"""
+## divide function below: -> CORRECT THIS FUNTION WITH TRY/EXCEPT BLOCKS THEN REMOVE QUOTATIONS WITHIN EACH FUNCTION TO START TEST
+
+def divide():
+    num1 = int(input("Enter a number: "))
+    num2 = int(input("Enter another number: "))
+
+    div = num1 / num2
+
+    print("Your numbers divided is:", div)
+
+## divide test function with 2 distinct tests (math correctness & data type) below:
+
+## Math Correctness: the first 2 tests-> 1st fails/2nd passes
+## Data Types: the last 4 tests-> 3rd passes/4th fails/5th fails/6th passes
+## (Data Types used: boolean/strings/floats/integers)
+
+def test_divide():
+    num1 = iter([2])
+    num2 = iter([2])
+    assert divide == 3
+
+
+def test_divide2():
+    num1 = iter([50])
+    num2 = iter([2])
+    assert divide
+
+def test_divide3():
+    num1 = iter([True])
+    num2 = iter([True])
+    assert divide
+
+def test_divide4():
+    num1 = iter(["0"])
+    num2 = iter(["4"])
+    assert divide == 0
+
+def test_divide5():
+    num1 = iter([100.0])
+    num2 = iter([5.0])
+    assert divide
+
+def test_divide5():
+    num1 = iter([7])
+    num2 = iter([7])
+    assert divide == 1
+"""
 ##--------------------------------------------------------------------------
+"""
+## sq function below: -> CORRECT THIS FUNTION WITH TRY/EXCEPT BLOCKS THEN REMOVE QUOTATIONS WITHIN EACH FUNCTION TO START TEST
+
+def sq(num):
+    return math.sqrt(num)
+
+## sq test function with 2 distinct tests (math correctness & data type) below:
+
+## Math Correctness: the first 2 tests-> 1st passes/2nd fails
+## Data Types: the last 5 tests-> 3rd fails/4th passes/5th passes/6th passes
+## (Data Types used: string/boolean/float/integer)
+
+def test_sq():
+    assert sq(49) == 7
+
+def test_sq2():
+    assert sq(49) == 9
+
+def test_sq3():
+    assert sq("100") == 10
+
+def test_sq4():
+    assert sq(False) == 0
+
+def test_sq5():
+    assert sq(144.0) == 12.0
+
+def test_sq6():
+    assert sq(36) == 6
+"""
+##--------------------------------------------------------------------------
+"""
+## greetUser function below:  -> CORRECT THIS FUNTION WITH TRY/EXCEPT BLOCKS THEN REMOVE QUOTATIONS WITHIN EACH FUNCTION TO START TEST
+
+def greetUser(first, middle, last):
+    print("Hello!")
+    print("Welcome to the program", first, middle, last)
+    print("Glad to have you!")
+
+## greetUser test function with 1 distinct test (data/input correctness) below:
+
+## Input Correctness: 1st passes/2nd passes/3rd passes/4th passes/5th passes/6th fails
+
+def test_greetUser():
+    greetUser("Javier", "Ivan", "Davis")
+
+def test_greetUser2(capsys):
+    greetUser("Javier", "Ivan", "Davis")
+
+    captured_stdout, captured_stderr = capsys.readouterr()
+    assert captured_stdout.strip()
+       
+def test_greetUser3():
+    greetUser("N1yana", "Ama!ya", "C0hran")
+
+def test_greetUser4():
+    greetUser("Javier", "1van", "Davis")   
+
+def test_greetUser5():
+    greetUser("Jayce", "King", "Davis")
+
+def test_greetUser6(capsys):
+    first = (["Javier"])
+    middle = (["Ivan"])
+    last =  (["Davis"])
+    assert greetUser == "Glad to have you!"
+"""
+##--------------------------------------------------------------------------
+"""
+## displayItem function below:  -> CORRECT THIS FUNTION WITH TRY/EXCEPT BLOCKS THEN REMOVE QUOTATIONS WITHIN EACH FUNCTION TO START TEST
+
+def displayItem(numbers, index):
+    print("Your item at", index, "index is", numbers[index]) 
+
+## displayItem test function with 2 distinct tests (order correctness & data type) below:
+
+## Order Correctness: the first, second & last test-> 1st passes/2nd passes/6th fails
+## Data Types: the middle tests-> 3rd passes/4th passes/5th passes
+## (Data Types used: floats/strings/boolean)
+
+def test_displayItem(capsys):
+    numbers = iter([1, 2, 3, 4, 5, 6])
+    index = iter([5])
+    assert displayItem
+
+def test_displayItem2(capsys):
+    numbers = iter([32, 43, 96, 674, 35, 36])
+    index = iter([16])
+    assert displayItem
+
+def test_displayItem3(capsys):
+    numbers = iter([12, 23, 36, 44, 25, 56.3])
+    index = iter([6.0])
+    assert displayItem
+
+def test_displayItem4(capsys):
+    numbers = iter(["s", 23, 36, 44, 25, 22])
+    index = iter(["b"])
+    assert displayItem
+
+def test_displayItem5(capsys):
+    numbers = iter([12, 23, 36, 44, 25, 56, 60])
+    index = iter([True])
+    assert displayItem
+
+def test_displayItem6(capsys):
+    numbers = iter([1, 2, 3, 4, 5, 6])
+    index = iter([2])
+    assert displayItem == "Your item at 2 index is 6"
+"""
+##-------------------------------------------------------------------------- STOP CORRECTING JD!
 
 ## below are the given functions to test
 
--- test_functions.py
+++ test_functions.py
@@ -1,5 +1,5 @@
 """
-Created by: Kennedy Keyes and Javier Davis (add netIDs!)
+Created by: Kennedy Keyes (kfk38) and Javier Davis (insert netID)
 Assignment: Software Testing Group Project
 Course: CSE Methods and Tools in Software Development
 Program: Creation of test functions
@@ -90,13 +90,173 @@ def isPalindrome(temp):
 ## isPalindrome test function with 2 distinct tests (palindrome correctness & data type) below:
 
 ## Palindrome Correctness: the first 2 tests-> 1st passes/2nd fails
-## Data Types: the last 4 tests-> 3rd fails/4th fails/5th fails/6th fails/7th passes
+## Data Types: the last 5 tests-> 3rd fails/4th fails/5th fails/6th fails/7th passes
 ## (Data Types used: integer/boolean/complex number/float/string)
 
 @pytest.mark.parametrize("strings, boolean", [("racecar", True), ("anna", False), 
 ("101", True), (True, True), (3j, False), (10.3, False), ("cat", False)])
 def test_isPalindrome(strings, boolean):
     assert isPalindrome(strings) == boolean
+
+##--------------------------------------------------------------------------
+
+## divide function below:
+
+def divide():
+    num1 = int(input("Enter a number: "))
+    num2 = int(input("Enter another number: "))
+
+    div = num1 / num2
+
+    print("Your numbers divided is:", div)
+
+## divide test function with 2 distinct tests (math correctness & data type) below:
+
+## Math Correctness: the first 2 tests-> 1st fails/2nd passes
+## Data Types: the last 4 tests-> 3rd passes/4th fails/5th fails/6th passes
+## (Data Types used: boolean/strings/floats/integers)
+
+def test_divide():
+    num1 = iter([2])
+    num2 = iter([2])
+    assert divide == 3
+
+
+def test_divide2():
+    num1 = iter([50])
+    num2 = iter([2])
+    assert divide
+
+def test_divide3():
+    num1 = iter([True])
+    num2 = iter([True])
+    assert divide
+
+def test_divide4():
+    num1 = iter(["0"])
+    num2 = iter(["4"])
+    assert divide == 0
+
+def test_divide5():
+    num1 = iter([100.0])
+    num2 = iter([5.0])
+    assert divide
+
+def test_divide5():
+    num1 = iter([7])
+    num2 = iter([7])
+    assert divide == 1
+
+##--------------------------------------------------------------------------
+
+## sq function below:
+
+def sq(num):
+    return math.sqrt(num)
+
+## sq test function with 2 distinct tests (math correctness & data type) below:
+
+## Math Correctness: the first 2 tests-> 1st passes/2nd fails
+## Data Types: the last 5 tests-> 3rd fails/4th passes/5th passes/6th passes
+## (Data Types used: string/boolean/float/integer)
+
+def test_sq():
+    assert sq(49) == 7
+
+def test_sq2():
+    assert sq(49) == 9
+
+def test_sq3():
+    assert sq("100") == 10
+
+def test_sq4():
+    assert sq(False) == 0
+
+def test_sq5():
+    assert sq(144.0) == 12.0
+
+def test_sq6():
+    assert sq(36) == 6
+
+##--------------------------------------------------------------------------
+
+## greetUser function below:
+
+def greetUser(first, middle, last):
+    print("Hello!")
+    print("Welcome to the program", first, middle, last)
+    print("Glad to have you!")
+
+## greetUser test function with 1 distinct test (data/input correctness) below:
+
+## Input Correctness: 1st passes/2nd passes/3rd passes/4th passes/5th passes/6th fails
+
+def test_greetUser():
+    greetUser("Javier", "Ivan", "Davis")
+
+def test_greetUser2(capsys):
+    greetUser("Javier", "Ivan", "Davis")
+
+    captured_stdout, captured_stderr = capsys.readouterr()
+    assert captured_stdout.strip()
+       
+def test_greetUser3():
+    greetUser("N1yana", "Ama!ya", "C0hran")
+
+def test_greetUser4():
+    greetUser("Javier", "1van", "Davis")   
+
+def test_greetUser5():
+    greetUser("Jayce", "King", "Davis")
+
+def test_greetUser6(capsys):
+    first = (["Javier"])
+    middle = (["Ivan"])
+    last =  (["Davis"])
+    assert greetUser == "Glad to have you!"
+
+##--------------------------------------------------------------------------
+
+## displayItem function below:
+
+def displayItem(numbers, index):
+    print("Your item at", index, "index is", numbers[index]) 
+
+## displayItem test function with 2 distinct tests (order correctness & data type) below:
+
+## Order Correctness: the first, second & last test-> 1st passes/2nd passes/6th fails
+## Data Types: the middle tests-> 3rd passes/4th passes/5th passes
+## (Data Types used: floats/strings/boolean)
+
+def test_displayItem(capsys):
+    numbers = iter([1, 2, 3, 4, 5, 6])
+    index = iter([5])
+    assert displayItem
+
+def test_displayItem2(capsys):
+    numbers = iter([32, 43, 96, 674, 35, 36])
+    index = iter([16])
+    assert displayItem
+
+def test_displayItem3(capsys):
+    numbers = iter([12, 23, 36, 44, 25, 56.3])
+    index = iter([6.0])
+    assert displayItem
+
+def test_displayItem4(capsys):
+    numbers = iter(["s", 23, 36, 44, 25, 22])
+    index = iter(["b"])
+    assert displayItem
+
+def test_displayItem5(capsys):
+    numbers = iter([12, 23, 36, 44, 25, 56, 60])
+    index = iter([True])
+    assert displayItem
+
+def test_displayItem6(capsys):
+    numbers = iter([1, 2, 3, 4, 5, 6])
+    index = iter([2])
+    assert displayItem == "Your item at 2 index is 6"
 
 ##--------------------------------------------------------------------------
 
@@ -140,6 +300,7 @@ def isPalindrome(temp):
     else:
         return False
 
+## COMPLETED -------------------------------------------------------------JD
 ## has input to receive two numbers
 ## divides the two, then outputs the result
 def divide():
@@ -150,10 +311,12 @@ def divide():
 
     print("Your numbers divided is:", div)
 
+## COMPLETED -------------------------------------------------------------JD
 ## returns the squareroot of a particular number
 def sq(num):
     return math.sqrt(num)
 
+## COMPLETED -------------------------------------------------------------JD
 ## grabs user's name
 ## greets them by their entire name
 ## names should be strings that only accept letters! (A-Z, a-z)
@@ -162,6 +325,7 @@ def greetUser(first, middle, last):
     print("Welcome to the program", first, middle, last)
     print("Glad to have you!")
 
+## COMPLETED -------------------------------------------------------------JD
 ## takes in a Python list
 ## attempts to display the item at the index provided
 def displayItem(numbers, index):
